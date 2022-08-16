5b43b39a2b9a0068bf3a926d3ff67f1b559687cc
myapos <myapos@gmail.com>
2022-08-17 02:16:44 +0300

    used a custom native module

--- screens/toneGenerator/components/ToneGenerator.js
+++ screens/toneGenerator/components/ToneGenerator.js
@@ -2,14 +2,9 @@ import { useState, useEffect } from "react";
 import { Text, View, Button } from "react-native";
 
 const ToneGenerator = ({ navigation }) => {
-  function play() {
-    console.log("play");
-  }
-
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>ToneGenerator Screen</Text>
-      <Button onPress={play} title="Play tone" />
     </View>
   );
 };
--- screens/tuner/components/Tuner.js
+++ screens/tuner/components/Tuner.js
@@ -1,9 +1,36 @@
-import { Text, View, Button } from "react-native";
+import React, { useState } from "react";
+
+import { Text, View, Button, NativeModules } from "react-native";
+// "ReactOneCustomMethod" it's the same value returned by getName()
+// from ReactOneCustomMethod
+// const { RNDtmf } = NativeModules;
+function play() {
+  console.log("play");
+}
 
 const Tuner = ({ navigation }) => {
+  const { ReactOneCustomMethod } = NativeModules;
+  const [id, setId] = useState("Press the button to get The ID");
+
+  console.log("ReactOneCustomMethod", ReactOneCustomMethod);
+  const getId = () => {
+    ReactOneCustomMethod.getPhoneID()
+      .then((res) => {
+        setId("ID: " + res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Tuner Screen</Text>
+      <Text>Tuner Screen!!</Text>
+
+      <Button onPress={play} title="Play tone" />
+      <Button title="Get Id" onPress={getId} />
+
+      <Text>{id}</Text>
     </View>
   );
 };
